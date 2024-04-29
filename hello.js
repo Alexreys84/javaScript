fa3a74c231d53aa7c6d1810485d5ef2d0e6d6f07
Alex Reis <alexreis1984@gmail.com>
2024-04-29 14:10:02 -0300

    javaScript

---
+++ hello.js
@@ -0,0 +1,24 @@
+/**
+ * Fundamentos
+ * Sintaxe Básica da Linguagem JavaScript
+ * @author Alex dos Reis 
+ */
+
+// sentenças
+console.log("Hello, World!\n")
+
+// Blocos
+{
+    console.log("Hello, JavaScript!\n")
+}
+
+// omissão de chaves em estruturas de controle
+let media = 7
+if (media >= 5) 
+    console.log("Aprovado!\n")
+    
+ else 
+    console.log("Reprovado!\n")
+
+for (let x = 1; x <= 10; x++)
+    console.log(x)
